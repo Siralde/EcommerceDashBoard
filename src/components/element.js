521fef24ad70fa4272c8607a39e5add6f6b1053f
Aldemaro González <aldemarogonzalez@Aldemaros-MacBook-Pro.local>
2021-02-04 15:16:51 +0100

    update wip

--- src/components/element.js
+++ src/components/element.js
@@ -6,23 +6,22 @@ import Plus from "../assets/images/add.svg";
 import Cancel from "../assets/images/cancel.png";
 import Check from "../assets/images/check.png";
 
-export function UpdateElement() 
+export function UpdateElement({id, header, primaryDescription, secundaryDescription, img, updateElement, url, cancelUpdate}) 
 {
     return (
         <div className="crud_element">
             <img src={Image} alt="" />
                 <div className="crud_element_body">
                 <div>
-                    <input></input> 
+                    <input defaultValue={header}></input> 
                     <img src={Image} alt="@some_lego_guy" />
                 </div>
-
-                <input></input>
-                <input></input>
+                <input defaultValue={primaryDescription}></input>
+                <input defaultValue={secundaryDescription}></input>
 
                 <div className="crud_element_interactions">    
-                    <img src={Check} alt="Check" />
-                    <img src={Cancel} alt="Check" />
+                    <img src={Check} alt="Check" onClick={()=> updateElement(url, id)} />
+                    <img src={Cancel} alt="Cancel" onClick={() => cancelUpdate(false)}/>
                 </div>
             </div>
         </div>
@@ -30,17 +29,15 @@ export function UpdateElement()
 };
 
 
-export function CreateElement() 
+export function CreateElement( {updateElement}) 
 {
     const [update, setUpdate] = useState(false)
 
-
     return (
         <div>
-
             {update 
                 ?
-                <UpdateElement/>
+                <UpdateElement updateElement={updateElement} cancelUpdate={setUpdate}/>
                 :
                 (<div className="new_element">
                     <img src={Plus} alt="Plus" onClick={ () => { setUpdate(!update) } } />
@@ -51,17 +48,25 @@ export function CreateElement()
     )
 };
 
-export function ReadElement({id, header, primaryDescription, secundaryDescription, img}) 
+export function ReadElement({id, header, primaryDescription, secundaryDescription, img, updateElement, deleteElement, url}) 
 {
 
     const [update, setUpdate] = useState(false)
 
     return (
         <div>
-
             {update 
                 ?
-                <UpdateElement/>
+                <UpdateElement
+                    id={id} 
+                    header={header} 
+                    primaryDescription={primaryDescription}
+                    secundaryDescription={secundaryDescription}
+                    img={id}
+                    updateElement={updateElement}
+                    url={url}
+                    cancelUpdate={setUpdate}
+                />  
                 :
                 (<div className="crud_element">
                     <img src={Image} alt="" />
@@ -77,7 +82,7 @@ export function ReadElement({id, header, primaryDescription, secundaryDescriptio
                         <div className="crud_element_interactions">  
                         
                             <img src={Edit} alt="Edit" onClick={() => setUpdate(!update)}/>
-                            <img src={Trash} alt="Trash"/>
+                            <img src={Trash} alt="Trash" onClick={() => deleteElement(url, id)}/>
                         </div>
                     </div>
                 </div>)
--- src/containers/ClientPage.js
+++ src/containers/ClientPage.js
@@ -1,28 +1,92 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Container } from 'semantic-ui-react';
 import useFetch from '../customHooks/fetchHook';
 import { ReadElement, CreateElement, UpdateElement } from '../components/element';
 
+const URL = "http://localhost:3001/clients/";
+
 function ClientPage(){
 
-    const [response, loading, hasError] = useFetch("http://localhost:3001/clients/")
+    // async function deleteElement(url = '') {
+    //     const response = await fetch(url, {
+    //       method: 'DELETE', 
+    //       headers: {
+    //         'Content-Type': 'application/json'
+    //       },
+    //     });
+
+
+    //     console.log(response.json());
+    //     return response.json(); // parses JSON response into native JavaScript objects
+    // }
     
+    const [response, loading, hasError] = useFetch(URL)
+
+    const [clients, setClients] = useState(response);
+
+    useEffect( () => { setClients(response) }, [response]);
+
+    const deleteElement = useCallback(
+        (url, id) => {
+            fetch(url+id, {
+                method: 'DELETE', 
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then((res) => {
+                res.json().then((data) => {
+                    setClients( clients.filter((client) => {
+                        return client.id !==id
+                    }))
+                })
+            })
+            .catch(error => {console.log(error);
+            })
+        },
+        [clients]
+    )
+    
+    const updateElement = useCallback(
+        (url, id) => {
+
+            console.log("Update");
+            // fetch(url+id, {
+            //     method: 'DELETE', 
+            //     headers: {
+            //         'Content-Type': 'application/json'
+            //     }
+            // })
+            // .then((res) => {
+            //     res.json().then((data) => {
+            //         setClients( clients.filter((client) => {
+            //             return client.id !==id
+            //         }))
+            //     })
+            // })
+            // .catch(error => {console.log(error);
+            // })
+        }
+        ,
+        [clients]
+    )
+
     return (
         <Container>
             
-            <CreateElement/>
+            <CreateElement updateElement={updateElement}/>
             
             {loading 
                 ? 
                 <div>Loading...</div> 
-                : 
+                :
                 (hasError 
                     ? 
                     <div>Error occured.</div> 
                     : 
-                    (response 
+                    (clients 
                         ?
-                        response.map((data, index) => {              
+                        clients.map((data, index) => {              
                             return (
                                 <ReadElement 
                                     key={index}
@@ -31,6 +95,9 @@ function ClientPage(){
                                     primaryDescription={data.email}
                                     secundaryDescription={data.telephone}
                                     img={data.id}
+                                    deleteElement={deleteElement}
+                                    updateElement={updateElement}
+                                    url={URL}
                                 />
                             )
                         })
